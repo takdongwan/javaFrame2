4bfc75c4b6eb6f9fb95dc8aa3754b9b0cb585a62
takdongwan <tak46123@gamil.com>
2020-04-01 21:59:06 +0900

    pull 어떻게하지

---
+++ musicgame/src/dynamic_beat_7/Main.java
@@ -0,0 +1,15 @@
+package dynamic_beat_7;
+//시작화면 메뉴바 ,
+public class Main {
+
+	public static final int SCREEN_WIDTH = 1280;//한번선언하면 안바구 ㅣ는 상수는 대문자로
+	public static final int SCREEN_HEIGHT= 720;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		new DynamicBeat();
+	
+	}
+
+}
