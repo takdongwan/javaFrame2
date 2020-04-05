32e47ff5b328ace6338f9d9fceccfca5d61ce8d3
takdongwan <tak46123@gamil.com>
2020-04-06 05:16:26 +0900

    게임화면 틀 구성 ,디자인적요소

---
+++ musicgame/src/music_game_10/MusicTrack.java
@@ -0,0 +1,54 @@
+package music_game_10;
+
+public class MusicTrack {
+	
+	
+	private String titleImage ;// 제목이미지
+	private String startImage; // 게임 선택시 창 표지 이미지
+	private String gameImage; // 해당 곡을 실행했을때 표지이미지
+	private String startMusic; // 게임선택 창 음악
+	private String gameMusic; // 해당 곡을 실행했을 때 음악 
+	public String getTitleImage() {
+		return titleImage;
+	}
+	public void setTitleImage(String titleImage) {
+		this.titleImage = titleImage;
+	}
+	public String getStartImage() {
+		return startImage;
+	}
+	public void setStartImage(String startImage) {
+		this.startImage = startImage;
+	}
+	public String getGameImage() {
+		return gameImage;
+	}
+	public void setGameImage(String gameImage) {
+		this.gameImage = gameImage;
+	}
+	public String getStartMusic() {
+		return startMusic;
+	}
+	public void setStartMusic(String startMusic) {
+		this.startMusic = startMusic;
+	}
+	public String getGameMusic() {
+		return gameMusic;
+	}
+	public void setGameMusic(String gameMusic) {
+		this.gameMusic = gameMusic;
+	}
+	
+	
+	public MusicTrack(String titleImage, String startImage, String gameImage, String startMusic, String gameMusic) {
+		super();
+		this.titleImage = titleImage;
+		this.startImage = startImage;
+		this.gameImage = gameImage;
+		this.startMusic = startMusic;
+		this.gameMusic = gameMusic;
+	}
+	
+	
+
+}
